3832efc3caf7070bbd0ce5920e89a66fa6363e23
Jonathan Mohabir <jonamohabir416@gmail.com>
2023-01-22 16:08:51 -0500

    new commit, Beans concept understood, Annotations, and Controllers understood. Home page route created using getMapping(/), move onto lab 02

--- src/main/java/com/cpan252/tekkenreborn/TekkenrebornApplication.java
+++ src/main/java/com/cpan252/tekkenreborn/TekkenrebornApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TekkenrebornApplication {
 
+  // main method to start off the application
+  public static void main(String[] args) {
+    SpringApplication.run(TekkenrebornApplication.class, args);
 
-    // main method to start off the application
-	public static void main(String[] args) {
-		SpringApplication.run(TekkenrebornApplication.class, args);
-	}
+  }
+  // this gets all the arguments from the command line and passes them to the
+  // SpringApplication.run method.
 
 }
--- src/main/java/com/cpan252/tekkenreborn/controller/HomeController.java
+++ src/main/java/com/cpan252/tekkenreborn/controller/HomeController.java
@@ -1,19 +1,27 @@
+/*.
+ *Code Base for Tekken Reborn Application.
+  Jonathan Mohabir
+ 
+  *New code written on test Branch, then merged on MASTER. 
+  Refer to comments to see what code is being written, for 
+  each lab. 
+ */
 package com.cpan252.tekkenreborn.controller;
-
 import org.springframework.stereotype.Controller;
+// ensure the controller component is imported. 
+import org.springframework.web.bind.annotation.GetMapping;
 
-
-//Controller for the home page
 @Controller
 public class HomeController {
-
-
-    // Method to return the home page
-    public String home() {
-        return "home";
-    }
-
-
-
-    
+  // this is the controller class.
+  @GetMapping("/")
+  public String home() {
+    return "home";
+  }
+  //this method returns a home page with the message Home.
 }
+
+/*
+ * The controller acts as a dispatcher for the application. 
+ * Endpoint is the URL that the user will type in to access the application.
+ */
