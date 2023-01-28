b741f9debaabc09525b0d7ad7d43e5206f5a13ff
Jonathan Mohabir <jonamohabir416@gmail.com>
2023-01-28 12:41:51 -0500

    Controller class understood, HomeController created and routing to the home.html page, about.html page added but no route defined in a controller class

--- src/main/java/com/cpan252/tekkenreborn/TekkenrebornApplication.java
+++ src/main/java/com/cpan252/tekkenreborn/TekkenrebornApplication.java
@@ -1,17 +1,12 @@
 package com.cpan252.tekkenreborn;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class TekkenrebornApplication {
 
-  // main method to start off the application
+  //MAIN METHOD TO START APP. 
   public static void main(String[] args) {
     SpringApplication.run(TekkenrebornApplication.class, args);
 
   }
-  // this gets all the arguments from the command line and passes them to the
-  // SpringApplication.run method.
-
 }
