7072f7664eda81c311f3ea1eba1c7e7fe77839da
Jonathan Mohabir <jonamohabir416@gmail.com>
2023-01-28 13:53:46 -0500

    updated front end styling

--- src/main/java/com/cpan252/tekkenreborn/TekkenrebornApplication.java
+++ src/main/java/com/cpan252/tekkenreborn/TekkenrebornApplication.java
@@ -1,10 +1,12 @@
 package com.cpan252.tekkenreborn;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class TekkenrebornApplication {
 
-  //MAIN METHOD TO START APP. 
+  // MAIN METHOD TO START APP.
   public static void main(String[] args) {
     SpringApplication.run(TekkenrebornApplication.class, args);
 
