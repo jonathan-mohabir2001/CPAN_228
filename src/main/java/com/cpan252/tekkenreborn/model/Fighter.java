5b9f2ef93ad1a0c8d316b0a4d0786aafc288791a
Jonathan Mohabir <jonamohabir416@gmail.com>
2023-01-30 10:36:45 -0500

    Front end form for adding fighter added, and styled.

--- src/main/java/com/cpan252/tekkenreborn/model/Fighter.java
+++ src/main/java/com/cpan252/tekkenreborn/model/Fighter.java
@@ -26,7 +26,8 @@ public class Fighter {
   private int damagePerHit;
   private int health;
   private double resistance;
-  private Anime animeForm;
+  private Anime animeFrom;
+  // this Anime enum was changed to correct spelling of from.
 
   public enum Anime {
     NARUTO("Naruto"), BLEACH("Bleach"), ONE_PIECE("One Piece"), TEKKEN("Tekken");
